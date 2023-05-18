6d6d7afd9bfe81000777dbfeae690322a8a2b5e8
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-05-17 22:23:37 -0500

    done 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word,nums) {
+    let answer = "";
+    if (nums < 0){
+        return 'ERROR';
+    }
+    for(let x = 0; x<nums; x++){
+        answer += word;        
+    }
+    return answer;
 };
 
 // Do not edit below this line
