53c56a24746f128f2d996e867bb150dd4abccded
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-05-17 22:38:17 -0500

    fone 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let answer = "";
+    for(let x = string.length -1; x >= 0; x--){
+        answer += string.charAt(x);
+    }
+    return answer;
 };
 
 // Do not edit below this line
