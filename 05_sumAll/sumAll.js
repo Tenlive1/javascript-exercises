394f9aa2010dd02d99b8d7ae2f8a30dc1317351b
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-05-17 23:43:39 -0500

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let answer = 0;
+    if(typeof num1 == 'number' && typeof num2 == 'number' && num1 >= 0 && num2 >= 0){
+        let great = 0;
+        let least = 0;
+        if (num1 > num2){
+            great = num1;
+            least = num2;
+        }else{
+            great = num2;
+            least = num1; 
+        }
+        for (let x = least; x <= great; x++){
+            answer += x;
+        }
+    }
+    else 
+    return 'ERROR';
 
+    return answer;
 };
 
 // Do not edit below this line
