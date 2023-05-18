c3d93c4a8719b56db1d1a188dc03469d1c843dec
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-05-17 23:26:43 -0500

    done 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrays, ...remove) {
+    let answer = [];
+    let position =0;
+    for(let x =0; x < arrays.length; x++){
+        let z =0;
+        let word = arrays[x];
+        for(let y =0; y<remove.length; y++){
+            if(word === remove[y]){
+                z++;
+                break;
+            }
+        }
+        if(z === 0){
+            answer[position] = word;
+            position++;
+        }
+    }
+    return answer
 };
 
 // Do not edit below this line
